30a908d2e858c96575f3681381ff75bc6220cca8
Shadi Abureesh <157613684+ShadiAbureesh@users.noreply.github.com>
2024-07-09 20:39:33 +0300

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -1,6 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
-    console.log("navbar.js loaded"); // Add this line
-
     const navbarHTML = `
         <nav class="bg-white dark:bg-gray-800 shadow">
             <div class="container mx-auto px-4">
